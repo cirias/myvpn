0ab963939c13c202948e2154a5b61e29e1ac13d9
Sirius <fhc023@gmail.com>
2016-07-06 15:20:54 +0800

    update client and server

--- myvpn-client/main.go
+++ myvpn-client/main.go
@@ -1,32 +1,17 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"net"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
-	"protocol"
-	"tun"
-
+	"github.com/cirias/myvpn/socket"
+	"github.com/cirias/myvpn/tun"
+	"github.com/cirias/myvpn/vpn"
 	"github.com/golang/glog"
 )
-
-func dial(network, secret, serverAddr string) (conn protocol.Conn, err error) {
-	switch network {
-	case "udp":
-		err = errors.New("UDP has not been implement")
-	case "tcp":
-		conn, err = protocol.DialTCP(secret, serverAddr)
-	default:
-		err = errors.New("unknown protocol")
-	}
-
-	return
-}
 
 func main() {
 	var network, secret, serverAddr, upScript, downScript, addrScript string
@@ -39,12 +24,11 @@ func main() {
 	flag.StringVar(&addrScript, "addr-script", "./if-addr.sh", "script file path for set ip address")
 	flag.Parse()
 
-	conn, err := dial(network, secret, serverAddr)
+	sock, err := socket.NewClient(secret, serverAddr)
 	if err != nil {
 		glog.Fatalln(err)
 	}
-	defer conn.Close()
-	glog.Infoln("connected to server", conn.LocalIPAddr())
+	defer sock.Close()
 
 	tun, err := tun.NewTUN("")
 	if err != nil {
@@ -52,106 +36,34 @@ func main() {
 	}
 	defer tun.Close()
 
-	err = tun.Run(addrScript, (&net.IPNet{conn.LocalIPAddr(), conn.IPNetMask()}).String())
+	client, err := vpn.NewClient(tun, sock)
 	if err != nil {
 		glog.Fatalln(err)
 	}
-	err = tun.Run(upScript, conn.ExternalRemoteIPAddr().String())
+	defer client.Close()
+
+	glog.Infoln("connected to server")
+
+	err = tun.Run(addrScript, (&net.IPNet{client.IP, client.IPMask}).String())
 	if err != nil {
 		glog.Fatalln(err)
 	}
-	defer tun.Run(downScript, (&net.IPNet{conn.LocalIPAddr(), conn.IPNetMask()}).String(), conn.ExternalRemoteIPAddr().String())
+	/*
+	 * err = tun.Run(upScript, conn.ExternalRemoteIPAddr().String())
+	 * if err != nil {
+	 *   glog.Fatalln(err)
+	 * }
+	 * defer tun.Run(downScript, (&net.IPNet{conn.LocalIPAddr(), conn.IPNetMask()}).String(), conn.ExternalRemoteIPAddr().String())
+	 */
 	glog.Infoln(tun.Name(), "is ready")
 
-	errc := make(chan error)
-
+	signalCh := make(chan os.Signal)
 	go func() {
-		b := make([]byte, 65535)
-		for {
-			var n int
-
-			for {
-				n, err = conn.ReadIPPacket(b)
-				if err == nil {
-					break
-				}
-				conn.Close()
-
-				for {
-					conn, err = dial(network, secret, serverAddr)
-					if err != nil {
-						time.Sleep(10 * time.Second)
-						continue
-					}
-					err = tun.Run(addrScript, (&net.IPNet{conn.LocalIPAddr(), conn.IPNetMask()}).String())
-					if err != nil {
-						glog.Warningln(err)
-					}
-					break
-				}
-			}
-
-			/*
-			 * n, err := conn.ReadIPPacket(b)
-			 * if err != nil {
-			 *   glog.Errorln("fail to read from connection", err)
-			 *   errc <- err
-			 *   continue
-			 * }
-			 */
-			glog.V(3).Infoln("recieve IP packet from connection", b[:n])
-
-			_, err = tun.Write(b[:n])
-			if err != nil {
-				glog.Errorln("fail to write to tun", err)
-				errc <- err
-			}
-			glog.V(3).Infoln("send IP packet to tun", b[:n])
-		}
-	}()
-	glog.Infoln("start processing data from connection")
-
-	go func() {
-		b := make([]byte, 65535)
-		for {
-			n, err := tun.ReadIPPacket(b)
-			if err != nil {
-				glog.Errorln("fail to read from tun", err)
-				errc <- err
-				continue
-			}
-			glog.V(3).Infoln("recieve IP packet from tun", b[:n])
-
-			for {
-				_, err = conn.Write(b[:n])
-				if err == nil {
-					break
-				}
-				time.Sleep(10 * time.Second)
-			}
-			/*
-			 * if err != nil {
-			 *   glog.Errorln("fail to write to connection", err)
-			 *   errc <- err
-			 * }
-			 */
-			glog.V(3).Infoln("send IP packet to connection", b[:n])
-		}
-	}()
-	glog.Infoln("start processing data from tun")
-
-	go func() {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt, os.Kill, syscall.SIGTERM)
-
-		s := <-c
-		errc <- errors.New(s.String())
+		signal.Notify(signalCh, os.Interrupt, os.Kill, syscall.SIGTERM)
 	}()
 
-	err = <-errc
-	if err != nil {
-		glog.Infoln("process quit", err)
-	}
+	sgn := <-signalCh
+	glog.Info("process quit", sgn)
 
 	return
 }
--- myvpn-server/main.go
+++ myvpn-server/main.go
@@ -1,37 +1,22 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"net"
 	"os"
 	"os/signal"
 	"syscall"
 
-	"protocol"
-	"tun"
-
+	"github.com/cirias/myvpn/socket"
+	"github.com/cirias/myvpn/tun"
+	"github.com/cirias/myvpn/vpn"
 	"github.com/golang/glog"
 )
-
-func listen(network, secret, listenAddr string, ip net.IP, ipNet *net.IPNet) (ln protocol.Listener, err error) {
-	switch network {
-	case "udp":
-		// ln, err = protocol.ListenUDP(secret, listenAddr, ip, ipNet)
-		err = errors.New("UDP has not been implement")
-	case "tcp":
-		ln, err = protocol.ListenTCP(secret, listenAddr, ip, ipNet)
-	default:
-		err = errors.New("unknown protocol")
-	}
-
-	return
-}
 
 func main() {
 	var network, secret, listenAddr, ipnet, upScript, downScript string
 
-	flag.StringVar(&network, "network", "udp", "network of transport layer")
+	flag.StringVar(&network, "network", "tcp", "network of transport layer")
 	flag.StringVar(&secret, "secret", "", "secret")
 	flag.StringVar(&listenAddr, "listen-addr", "0.0.0.0:9525", "listening address")
 	flag.StringVar(&ipnet, "ipnet", "10.0.200.1/24", "internal ip net")
@@ -39,15 +24,16 @@ func main() {
 	flag.StringVar(&downScript, "down-script", "./if-down.sh", "down shell script file path")
 	flag.Parse()
 
-	ip, ipNet, err := net.ParseCIDR(ipnet)
-	if err != nil {
-		glog.Fatalln(err)
-	}
-	ln, err := listen(network, secret, listenAddr, ip, ipNet)
+	sockServer, err := socket.NewServer(secret, listenAddr)
 	if err != nil {
 		glog.Fatalln(err)
 	}
 	glog.Infoln("start listening")
+
+	ip, ipNet, err := net.ParseCIDR(ipnet)
+	if err != nil {
+		glog.Fatalln(err)
+	}
 
 	tun, err := tun.NewTUN("")
 	if err != nil {
@@ -62,33 +48,31 @@ func main() {
 	defer tun.Run(downScript, (&net.IPNet{ip, ipNet.Mask}).String(), listenAddr)
 	glog.Infoln(tun.Name(), " is ready")
 
-	s := NewServer(tun, ipNet)
+	s, err := vpn.NewServer(tun, ipnet)
+	if err != nil {
+		glog.Fatalln(err)
+	}
 	defer s.Close()
 
-	errc := make(chan error)
-
+	signalCh := make(chan os.Signal)
 	go func() {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt, os.Kill, syscall.SIGTERM)
-
-		s := <-c
-		errc <- errors.New(s.String())
+		signal.Notify(signalCh, os.Interrupt, os.Kill, syscall.SIGTERM)
 	}()
 
 	go func() {
 		for {
-			c, err := ln.Accept()
+			sock, err := sockServer.Accept()
 			if err != nil {
 				glog.Errorln("fail to accept", err)
 			}
 
-			go s.Handle(c)
+			go s.Handle(sock)
 		}
 	}()
 	glog.Infoln("waiting client")
 
-	err = <-errc
-	glog.Info("process quit", err)
+	sgn := <-signalCh
+	glog.Info("process quit", sgn)
 
 	return
 }
